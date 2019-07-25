c519f6e3aae9e9dfd0a78783cd4bae2a0881a988
eralpkor <eralp.ny@gmail.com>
2019-07-25 15:58:04 -0400

    started creating Lambda classess

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,52 @@
 // CODE here for your Lambda Classes
+// base-class Person
+class Person {
+  constructor(props) {
+    this.name = props.name;
+    this.age = props.age;
+    this.location = props.location;
+  }
+
+  speak() {
+    `Hello my name is ${this.name}, I am from ${this.location}.`;
+  }
+}
+
+// Instructors goes here
+class Instructor extends Person {
+  constructor(props) {
+    super(props);
+    this.specialty = props.specialty;
+    this.favLanguage = props.favLanguage;
+    this.catchPhrase = props.catchPhrase;
+  }
+
+  demo(subject) {
+    console.log(`Today we are learning about ${subject}.`);
+  }
+
+  grade(student, subject) {
+    console.log(`${student.name} receives a perfect score on ${subject}.`);
+  }
+}
+
+// Student class
+class Student extends Person {
+  constructor(props) {
+    super(props);
+    this.previousBackground = props.previousBackground;
+    this.className = props.className;
+    this.favSubjects = props.favSubjects;
+  }
+}
+
+
+// 
+const fred = new Instructor({
+  name: 'Fred',
+  location: 'Bedrock',
+  age: 37,
+  favLanguage: 'JavaScript',
+  specialty: 'Front-end',
+  catchPhrase: `Don't forget the homies`
+});
